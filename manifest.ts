285f447f672c8fa637e1fc8260328764e684cc22
ricardo <ricardodeveloper03@gmail.com>
2025-05-12 19:02:42 +0200

    feat: :sparkles: add pwa config

---
+++ manifest.ts
@@ -0,0 +1,23 @@
+import type { ManifestOptions } from 'vite-plugin-pwa';
+
+export const manifest: Partial<ManifestOptions> = {
+  name: 'BBVA Memory Cards',
+  short_name: 'MemCards',
+  description: 'A simple game about memmory cards',
+  theme_color: '#4488EE',
+  background_color: '#F8F8F0',
+  display: 'standalone',
+  lang: 'es',
+  icons: [
+    {
+      src: '/favicon/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: '/favicon/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+  ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
+import { manifest } from './manifest';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest,
+      workbox: {
+        globDirectory: 'dist/',
+        globPatterns: ['**/*.{html,js,css,woff,woff2,ttf,eot,ico}'],
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:woff|woff2|ttf|eot|ico)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'fonts',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+        navigateFallback: null,
+      },
+    })
+  ],
 
 });
