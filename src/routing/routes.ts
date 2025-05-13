e41f9b014359cdc33af933e1eda9b599fa16fbc6
ricardo <ricardodeveloper03@gmail.com>
2025-05-13 18:06:24 +0200

    feat: define static routes

--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -1,12 +1,32 @@
 import { Route } from '@vaadin/router';
 
+import '@/module/layout/bbva-layout/bbva-layout.view';
+import '@/module/home/bbva-home/bbva-home.view';
+import '@/module/game/bbva-game/bbva-game.view';
+
+// Antes estaba con lazy loading, pero debido al requisito
+// de que funcionase sin conexión, se descartó esa opción
+
 export const ROUTES: Route[] = [
   {
     path: '/',
     component: 'bbva-layout',
     name: 'Memmory Cards',
-    action: async () => {
-      await import('@/module/layout/bbva-layout/bbva-layout.view');
-    },
+
+    children: [
+      {
+        path: '',
+        component: 'bbva-home',
+      },
+      {
+        path: 'game',
+        component: 'bbva-game',
+      },
+    ],
+  },
+
+  {
+    path: '(.*)',
+    redirect: '/',
   },
 ];
